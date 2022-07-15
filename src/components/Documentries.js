197cceb2191039d3f638b4f6aa6745a98a23b05b
prajith panicker <97846627+Prajithpanicker@users.noreply.github.com>
2022-07-15 16:50:17 +0530

    Update Documentries.js

--- src/components/Documentries.js
+++ src/components/Documentries.js
@@ -16,38 +16,38 @@ function Documentries() {
     <h4>New To Disney+</h4>
     <Content  {...settings}>
        <Wrap>
-            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/9885/1269885-v-38506a4291ec"/>
+            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/9885/1269885-v-38506a4291ec" alt=''/>
             
         </Wrap>
         <Wrap>
-            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8438/1208438-v-7007dde62b2d"/>
+            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8438/1208438-v-7007dde62b2d" alt=''/>
         </Wrap>
         <Wrap>
-            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/699/1240699-v-3f501146ce8d"/>
+            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/699/1240699-v-3f501146ce8d" alt=''/>
         </Wrap>
         <Wrap>
-            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/9475/1259475-v-44a5faa7c630"/>
+            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/9475/1259475-v-44a5faa7c630" alt=''/>
         </Wrap>
         <Wrap>
-            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/4557/1234557-v-6af995b46d98"/>
+            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/4557/1234557-v-6af995b46d98" alt=''/>
         </Wrap>
         <Wrap>
-            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/2235/1232235-v-6208310e6666"/>
+            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/2235/1232235-v-6208310e6666" alt=''/>
         </Wrap>
         <Wrap>
-            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5545/1195545-v-2781836a9a56"/>
+            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5545/1195545-v-2781836a9a56" alt=''/>
         </Wrap>
         <Wrap>
-            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7485/1117485-v-0b74106b75e5"/>
+            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7485/1117485-v-0b74106b75e5" alt=''/>
         </Wrap>
         <Wrap>
-            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6490/1086490-v-e906986f148f"/>
+            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6490/1086490-v-e906986f148f" alt=''/>
         </Wrap>
         <Wrap>
-            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/2671/1062671-v-63562bb16a0b"/>
+            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/2671/1062671-v-63562bb16a0b" alt=''/>
         </Wrap>
         <Wrap>
-            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5887/1085887-v-ff820302bf78"/>
+            <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5887/1085887-v-ff820302bf78" alt=''/>
         </Wrap> 
     </Content>
     </Container>
