6984004fc361ce9426e69e70137840c124669412
prajith panicker <97846627+Prajithpanicker@users.noreply.github.com>
2022-07-15 17:25:14 +0530

    Update Viewers.js

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -6,23 +6,23 @@ function Viewers() {
   return (
     <Container>
       <Wrap>
-        <img src=" /images/viewers-disney.png" />
+        <img src=" /images/viewers-disney.png" alt=""/>
         <video autoPlay muted loop src="/videos/1564674844-disney.mp4" />
       </Wrap>
       <Wrap>
-        <img src=" /images/viewers-pixar.png" />
+        <img src=" /images/viewers-pixar.png"alt="" />
         <video autoPlay muted loop src="/videos/1564676714-pixar.mp4" />
       </Wrap>
       <Wrap>
-        <img src=" /images/viewers-marvel.png" />
+        <img src=" /images/viewers-marvel.png" alt=""/>
         <video autoPlay muted loop src="/videos/1564676115-marvel.mp4" />
       </Wrap>
       <Wrap>
-        <img src=" /images/viewers-starwars.png" />
+        <img src=" /images/viewers-starwars.png"alt="" />
         <video autoPlay muted loop src="/videos/1608229455-star-wars.mp4" />
       </Wrap>
       <Wrap>
-        <img src=" /images/viewers-national.png" />
+        <img src=" /images/viewers-national.png" alt=""/>
         <video autoPlay muted loop src="/videos/1564676296-national-geographic.mp4" />
       </Wrap>
     </Container>
